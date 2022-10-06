8983cc05fd4182c4ce1a7adee9a57a78910d7b9c
Valdoveste <caio.valdoveste@outlook.com.br>
2022-10-06 13:16:30 -0300

    FreeCodeCampReactEx06

---
+++ src/components/Learn About Self-Closing JSX Tags.jsx
@@ -0,0 +1,9 @@
+const JSXSelfClossing = () => {
+    <div>
+        <h2>Welcome to React!</h2> <br />
+        <p>Be sure to close all tags!</p>
+        <hr />
+    </div>
+}
+
+export default JSXSelfClossing;
