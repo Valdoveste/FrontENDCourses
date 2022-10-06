42520c2e21d46e3693cfad31f8000bd6d843e868
Valdoveste <caio.valdoveste@outlook.com.br>
2022-10-06 13:24:15 -0300

    FreeCodeCampReactEx31

---
+++ src/components/Use the Lifecycle Method componentWillMount.jsx
@@ -0,0 +1,12 @@
+class MyCompnenet extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        console.log("log")
+    }
+    render() {
+        return <div />;
+    }
+}
