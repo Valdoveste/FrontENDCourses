f6613d54ba3a3eb0a453db8647c2c4c2c15d095c
Valdoveste <caio.valdoveste@outlook.com.br>
2022-10-06 13:15:52 -0300

    FreeCodeCampReactEx03

---
+++ src/components/Add Comments in JSX.jsx
@@ -0,0 +1,8 @@
+Create a Component with Compositionconst JSXComment = () =>
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+        {/* */}
+    </div>;
+
+export default JSXComment;
