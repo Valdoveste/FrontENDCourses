90ba590436f74852685cdbe60a2279e93c4f7fe4
Valdoveste <caio.valdoveste@outlook.com.br>
2022-10-11 12:38:08 -0300

    FreeCodeCampReactEx34

---
+++ src/components/Introductiong Inline Styles.jsx
@@ -0,0 +1,10 @@
+class Colorul extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <div style={{color: red, fontSize: bold}}>Bid Red</div>
+        );
+    }
+}
